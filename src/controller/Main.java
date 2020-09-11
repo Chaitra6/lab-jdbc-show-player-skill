a140acb969046d9df38452190e14e4d9368f09af
Chaitra6 <48185350+Chaitra6@users.noreply.github.com>
2020-09-11 10:18:37 +0530

    FSJ0820A003

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,1 +1,27 @@
+package controller;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import dao.SkillDAO;
+import model.Skill;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		
+		SkillDAO skillDAO = new SkillDAO();
+		List<Skill> skills;
+		
+		try {
+			skills = skillDAO.listAllSkills();
+			for(Skill skill : skills) {
+				System.out.println(skill.getSkilld()+"."+" "+skill.getSkillName());
+			}
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}	
+		
+
+	}
+}
--- src/dao/SkillDAO.java
+++ src/dao/SkillDAO.java
@@ -1,1 +1,47 @@
+package dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import utility.ConnectionManager;
+import model.Skill;
+
+public class SkillDAO {
+
+	public List<Skill> listAllSkills() throws SQLException, Exception {
+		String query = "SELECT * FROM SKILL ORDER BY SKILL_NAME ASC";
+		ConnectionManager con = new ConnectionManager();
+
+		List<Skill> list = new ArrayList<Skill>();
+		Skill skill = null;
+		ResultSet rs = null;
+
+		try {
+			Statement st = con.getConnection().createStatement();
+			rs = st.executeQuery(query);
+			while (rs.next()) {
+				skill = new Skill();
+
+				/*
+				 * Retrieve one skill details and store it in skill object
+				 */
+
+				skill.setSkilld(rs.getLong("SKILL_ID"));
+				skill.setSkillName(rs.getString("SKILL_NAME"));
+
+				// add each SKILL to the list
+				list.add(skill);
+			}
+
+		} finally {
+			con.getConnection().close();
+		}
+
+		return list;
+
+	}
+
+}
